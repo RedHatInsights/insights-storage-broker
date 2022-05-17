78cafe244880bf3a825d4c799d85c8ed514b66fa
Stephen Adams <stephen.adams@redhat.com>
2022-05-17 13:22:22 -0400

    [fix] Use full URL for storage endpoint
    Signed-off-by: Stephen Adams <stephen.adams@redhat.com>

--- src/storage_broker/utils/config.py
+++ src/storage_broker/utils/config.py
@@ -47,7 +47,7 @@ if os.getenv("ACG_CONFIG"):
     AWS_SECRET_ACCESS_KEY = cfg.objectStore.secretKey
     STAGE_BUCKET = ObjectBuckets[os.environ.get("PERM_BUCKET")].name
     REJECT_BUCKET = ObjectBuckets[os.environ.get("REJECT_BUCKET")].name
-    S3_ENDPOINT_URL = cfg.objectStore.hostname
+    S3_ENDPOINT_URL = f"http://{cfg.objectStore.hostname}:{cfg.objectStore.port}"
     # Logging
     CW_AWS_ACCESS_KEY_ID = os.getenv(
         "CW_AWS_ACCESS_KEY_ID", cfg.logging.cloudwatch.accessKeyId
