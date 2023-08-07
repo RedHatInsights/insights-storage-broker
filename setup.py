494b16fb2baedd0764a743b5c61728476fe45674
Tahmid Efaz <tefaz@redhat.com>
2023-08-07 18:21:15 -0400

    more dependency updates

--- setup.py
+++ setup.py
@@ -10,11 +10,11 @@ setup(
         "prometheus-client==0.7.1",
         "logstash-formatter==0.5.17",
         "boto3==1.28.21",
-        "watchtower==0.7.3",
+        "watchtower==3.0.1",
         "confluent-kafka==2.2.0",
         "pyyaml==6.0.1",
         "attrs==18.2.0",
-        "app-common-python==0.1.4"
+        "app-common-python==0.2.3"
     ],
     extras_require={"test": ["pytest>=5.4.1", "flake8>=3.7.9"]},
     entry_points={"console_scripts": ["storage_broker = storage_broker.app:main"]},
