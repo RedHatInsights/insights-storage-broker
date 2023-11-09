e382969bb3f96cc85583d6986cecf32c5d2dce86
Derek Horton <dehort@redhat.com>
2023-11-09 14:20:30 -0600

    maybe this explains what is happening a little better

--- src/storage_broker/mq/consume.py
+++ src/storage_broker/mq/consume.py
@@ -6,13 +6,16 @@ from src.storage_broker.utils import config
 def init_consumer(logger):
     logger.debug("initializing consumer")
     try:
-       connection_info = {
+       connection_info = _build_confluent_kafka_config(config)
+
+       consumer_config = {
                "group.id": config.APP_NAME,
                "queued.max.messages.kbytes": config.KAFKA_QUEUE_MAX_KBYTES,
                "enable.auto.commit": True,
                "allow.auto.create.topics": config.KAFKA_ALLOW_CREATE_TOPICS,
        }
-       connection_info.update(_build_confluent_kafka_config(config))
+
+       connection_info.update(consumer_config)
 
        consumer = Consumer(connection_info)
        logger.debug("Connected to consumer")
